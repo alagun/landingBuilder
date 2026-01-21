76fdf72811c6fafbb3b63b7c3f3851c0e1e48ff3
Alexey Lagun <alexey.lagun@gmail.com>
2026-01-21 16:50:05 +0300

    feat: canva обновление stage

--- src/entities/canvas/ui/CanvasStage.tsx
+++ src/entities/canvas/ui/CanvasStage.tsx
@@ -1,15 +1,52 @@
 'use client';
 
-import { Stage, Layer, Rect, Text } from 'react-konva';
-import { useState } from 'react';
+import { Stage, Layer, Text, } from 'react-konva';
+import { useSelector, useDispatch } from 'react-redux';
 
-interface ICanvasStageProps {
+import { CanvasImage } from './CanvasImage';
+import { CanvasProduct } from './CanvasProduct';
+import { selectElements, selectSelectedElementId, selectElement } from '../model/slices';
+import { CanvasElement } from '../model/types';
+
+interface CanvasStageProps {
   width?: number;
   height?: number;
 }
 
-export const CanvasStage = ({ width = 800, height = 600 }: ICanvasStageProps) => {
-  const [stageScale, setStageScale] = useState(1);
+export const CanvasStage = ({ width = 800, height = 600 }: CanvasStageProps) => {
+  const elements = useSelector(selectElements);
+  const selectedId = useSelector(selectSelectedElementId);
+  const dispatch = useDispatch();
+
+  const renderElement = (el: CanvasElement) => {
+    switch (el.type) {
+      case 'text':
+        return (
+          <Text
+            key={el.id}
+            x={el.x}
+            y={el.y}
+            text={el.text}
+            fontSize={el.fontSize}
+            fontFamily={el.fontFamily}
+            fill={el.fill}
+            rotation={el.rotation}
+            scaleX={el.scaleX}
+            scaleY={el.scaleY}
+            onClick={() => dispatch(selectElement(el.id))}
+          />
+        );
+
+      case 'image':
+        return <CanvasImage key={el.id} element={el} />;
+
+      case 'product':
+        return <CanvasProduct key={el.id} element={el} onClick={() => dispatch(selectElement(el.id))} />;
+
+      default:
+        return null;
+    }
+  };
 
   return (
     <div
@@ -20,10 +57,9 @@ export const CanvasStage = ({ width = 800, height = 600 }: ICanvasStageProps) =>
         margin: '20px auto',
       }}
     >
-      <Stage width={width} height={height} scaleX={stageScale} scaleY={stageScale}>
+      <Stage width={width} height={height}>
         <Layer>
-          <Rect x={50} y={50} width={100} height={50} fill="lightblue" />
-          <Text text="Hello Konva" x={200} y={100} fontSize={20} />
+          {elements.map(el => renderElement(el))}
         </Layer>
       </Stage>
     </div>
