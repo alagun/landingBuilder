379176138eb14b8c839b21eecbe953429e2d1a5b
Alexey Lagun <alexey.lagun@gmail.com>
2026-01-21 15:19:47 +0300

    fix: настроен начальный роутинг

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    reactStrictMode: true,
+  
+  /* Если нужно настроить алиасы для импортов */
+  // experimental: {
+  //   turbo: {
+  //     resolveAlias: {
+  //       '@': './src',
+  //     },
+  //   },
+  // },
 };
 
 export default nextConfig;
---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+// import './globals.css';
+import { StoreProvider } from './providers/StoreProvider';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <StoreProvider>{children}</StoreProvider>
+      </body>
+    </html>
+  );
+}
